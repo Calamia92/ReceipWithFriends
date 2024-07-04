15d7e3826a1c86c56240f8fa3ccbedd87657936e
Calamia92 <119439868+Calamia92@users.noreply.github.com>
2024-07-04 12:25:10 +0200

    MongoDB connexion

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '3000';
+
+//
+mongoose.connect(process.env.DATABASE_URL!, {})
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
