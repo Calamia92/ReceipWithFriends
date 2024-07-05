56fab260851811629491aab29263bd4c6f6bdd8d
Abondance <kazadiabondance@gmail.com>
2024-07-05 14:19:15 +0200

    my last change

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    NgClass,
   ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   email = new FormControl('', Validators.required);
   password = new FormControl('', Validators.required);
+  telephone = new FormControl('', Validators.required);
+  passwordHidden: Boolean | undefined;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.passwordHidden = false;
+  }
 
   registerCustomer() {
-    
+    console.log("On enregistre l'utilisateur");
+  }
+
+  viewPassword() {
+    this.passwordHidden = !this.passwordHidden;
   }
 
   onSubmit() {}
